c87ee83a63a9ee18f95584be95f0aa4f314933d6
Viacheslav Gerasimenko <supursoul@gmail.com>
2022-08-16 23:00:01 +0300

    Add show target component

---
+++ Life of Ozzy/Assets/Scripts/Components/Animations/ShowTargetComponent.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LifeOfOzzy.Components
+{
+    public class ShowTargetComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _delay = 0.5f;
+        [SerializeField] private UnityEvent _onDelay;
+        [SerializeField] private ShowTargetController _controller;
+
+        private Coroutine _coroutine;
+
+        private void OnValidate()
+        {
+            if (_controller == null)
+            {
+                _controller = FindObjectOfType<ShowTargetController>();
+            }
+        }
+
+        public void Play()
+        {
+            _controller.SetPosition(_target.position);
+            _controller.SetState(true);
+
+            if (_coroutine != null) StopCoroutine(_coroutine);
+
+            _coroutine = StartCoroutine(WaitAndReturn());
+        }
+
+        private IEnumerator WaitAndReturn()
+        {
+            yield return new WaitForSeconds(_delay);
+
+            _onDelay?.Invoke();
+            _controller.SetState(false);
+        }
+    }
+
+}
---
+++ Life of Ozzy/Assets/Scripts/Components/Animations/ShowTargetController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Cinemachine;
+
+namespace LifeOfOzzy.Components
+{
+    public class ShowTargetController : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private CinemachineVirtualCamera _camera;
+
+        private static readonly int ShowKey = Animator.StringToHash("show");
+
+        public void SetPosition(Vector3 targetPosition)
+        {
+            targetPosition.z = _camera.transform.position.z;
+            _camera.transform.position = targetPosition;
+        }
+
+        public void SetState(bool isShown)
+        {
+            _animator.SetBool(ShowKey, isShown);
+        }
+    }
+
+}
