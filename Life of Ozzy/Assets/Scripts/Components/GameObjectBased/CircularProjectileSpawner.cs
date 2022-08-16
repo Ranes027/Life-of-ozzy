d8ffb9b6fd808e4d18e0386e1bd1f1b45bf7278b
Viacheslav Gerasimenko <supursoul@gmail.com>
2022-08-17 00:34:21 +0300

    Update CircularProjectileSpawner.cs

--- Life of Ozzy/Assets/Scripts/Components/GameObjectBased/CircularProjectileSpawner.cs
+++ Life of Ozzy/Assets/Scripts/Components/GameObjectBased/CircularProjectileSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using LifeOfOzzy.Utils;
@@ -6,9 +7,9 @@ namespace LifeOfOzzy.Components
 {
     public class CircularProjectileSpawner : MonoBehaviour
     {
-        [SerializeField] private DirectionalProjectile _projectile;
-        [SerializeField] private int _burstCount;        
-        [SerializeField] private float _delay;
+        [SerializeField] private CircularProjectileSettings[] _settings;
+
+        public int Stage { get; set; }
 
         [ContextMenu("Launch")]
         public void LaunchProjectiles()
@@ -18,18 +19,30 @@ namespace LifeOfOzzy.Components
 
         private IEnumerator SpawnProjectiles()
         {
-            var sectorStep = 2 * Mathf.PI / _burstCount;
-            for (var i = 0; i < _burstCount; i++)
+            var settings = _settings[Stage];
+            var sectorStep = 2 * Mathf.PI / settings.BurstCount;
+            for (var i = 0; i < settings.BurstCount; i++)
             {
                 var angle = sectorStep * i;
                 var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
 
-                var instance = SpawnUtils.Spawn(_projectile.gameObject, transform.position);
+                var instance = SpawnUtils.Spawn(settings.Prefab.gameObject, transform.position);
                 var projectile = instance.GetComponent<DirectionalProjectile>();
                 projectile.Launch(direction);
-                yield return new WaitForSeconds(_delay);
+                yield return new WaitForSeconds(settings.Delay);
             }
         }
     }
 
+    [Serializable]
+    public struct CircularProjectileSettings
+    {
+        [SerializeField] private DirectionalProjectile _prefab;
+        [SerializeField] private int _burstCount;
+        [SerializeField] private float _delay;
+
+        public DirectionalProjectile Prefab => _prefab;
+        public int BurstCount => _burstCount;
+        public float Delay => _delay;
+    }
 }
