965bb0444e0faacadedc1207b845dbc176bfa414
Viacheslav Gerasimenko <supursoul@gmail.com>
2022-09-06 21:12:07 +0300

    Update LevelLoader

--- Life of Ozzy/Assets/Scripts/Components/LevelManagment/ExitLevelComponent.cs
+++ Life of Ozzy/Assets/Scripts/Components/LevelManagment/ExitLevelComponent.cs
@@ -7,12 +7,21 @@ namespace LifeOfOzzy.Components
     public class ExitLevelComponent : MonoBehaviour
     {
         [SerializeField] private string _sceneName;
-        
+        [Header("Level Name")]
+        [SerializeField] private bool _allowLevelName;
+        [SerializeField] private string _number;
+        [SerializeField] private string _title;
+
+        public bool AllowLevelName => _allowLevelName;
+
         public void Exit()
         {
             var session = FindObjectOfType<GameSession>();
             session.Save();
             var loader = FindObjectOfType<LevelLoader>();
+            if (_allowLevelName)
+                loader.SetLevelTitle(_number, _title);
+
             loader.LoadLevel(_sceneName);
         }
     }
--- Life of Ozzy/Assets/Scripts/UI/LevelsLoader/LevelLoader.cs
+++ Life of Ozzy/Assets/Scripts/UI/LevelsLoader/LevelLoader.cs
@@ -1,7 +1,10 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using LifeOfOzzy.Model;
 
 namespace LifeOfOzzy.UI
 {
@@ -9,8 +12,12 @@ namespace LifeOfOzzy.UI
     {
         [SerializeField] private Animator _animator;
         [SerializeField] private float _transitionTime;
+        [Header("Level Title")]
+        [SerializeField] private TextMeshProUGUI _levelNumber;
+        [SerializeField] private TextMeshProUGUI _levelTitle;
 
         private static readonly int Enabled = Animator.StringToHash("Enabled");
+        private static readonly int TitleKey = Animator.StringToHash("Title");
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         static void OnAfterSceneLoad()
@@ -36,9 +43,17 @@ namespace LifeOfOzzy.UI
         private IEnumerator StartAnimation(string sceneName)
         {
             _animator.SetBool(Enabled, true);
-            yield return new WaitForSeconds(_transitionTime);
+            yield return new WaitForSeconds(_transitionTime);            
             SceneManager.LoadScene(sceneName);
             _animator.SetBool(Enabled, false);
+        }
+
+        public void SetLevelTitle(string number, string title)
+        {
+            _levelNumber.text = number.ToString();
+            _levelTitle.text = LocalizationManager.I.Localize(title.ToString());
+
+            _animator.SetTrigger(TitleKey);
         }
     }
 
